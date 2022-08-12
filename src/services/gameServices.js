7a50a0d7843ac889d7dfb8a06f5509badce1f40e
vladislav99dev <vladislavcho99@abv.bg>
2022-08-12 14:20:31 +0300

    add getOne service

--- src/services/gameServices.js
+++ src/services/gameServices.js
@@ -3,4 +3,8 @@ let url = 'http://localhost:3030';
 export const getAll = () => {
   return  fetch(`${url}/data/games?sortBy=_createdOn%20desc`)
     .then((response) => response.json())
+} 
+export const getOne = (gameId) => {
+  return fetch(`${url}/data/games/${gameId}`)
+  .then((response) => response.json());
 }
