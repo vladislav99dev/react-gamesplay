861551c176c81c356729a146a7fc4ddcd07f2618
vladislav99dev <vladislavcho99@abv.bg>
2022-08-14 00:28:22 +0300

    update gameServices

--- src/services/gameServices.js
+++ src/services/gameServices.js
@@ -1,10 +1,12 @@
-let url = 'http://localhost:3030';
+import { url } from "../utils/constans";
 
 export const getAll = () => {
-  return  fetch(`${url}/data/games?sortBy=_createdOn%20desc`)
-    .then((response) => response.json())
-} 
+  return fetch(`${url}/data/games?sortBy=_createdOn%20desc`).then((response) =>
+    response.json()
+  );
+};
 export const getOne = (gameId) => {
-  return fetch(`${url}/data/games/${gameId}`)
-  .then((response) => response.json());
-}
+  return fetch(`${url}/data/games/${gameId}`).then((response) =>
+    response.json()
+  );
+};
