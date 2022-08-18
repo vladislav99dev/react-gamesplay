696064d88803d08238764fb501968f42146c2bfa
vladislav99dev <vladislavcho99@abv.bg>
2022-08-18 17:32:39 +0300

    useLocalStorage for data persistance

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+  const [state, setState] = useState(() => {
+    try {
+      let item = localStorage.getItem(key);
+
+      if (item) {
+        return JSON.stringify(item);
+      } else {
+        return initialValue;
+      }
+    } catch (err) {
+      console.log(err);
+      return initialValue;
+    }
+  });
+
+  const setItem = (value) => {
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+
+      setState(value);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return [state, setItem];
+};
+
+export default useLocalStorage;
