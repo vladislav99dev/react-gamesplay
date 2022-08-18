2625e7e0ea0abcadb7ba2be4bf265bc109ea531f
vladislav99dev <vladislavcho99@abv.bg>
2022-08-18 17:33:48 +0300

    authContext functions for fliud work

---
+++ src/context/AuthContext.js
@@ -0,0 +1,35 @@
+import { createContext, useContext } from "react";
+
+import useLocalStorage from "../hooks/useLocalStorage";
+
+const AuthContext = createContext();
+
+const initialValue = {
+  _id: "",
+  email: "",
+  accessToken: "",
+};
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useLocalStorage("user", initialValue);
+
+  const login = (authData) => {
+    setUser(authData);
+  };
+
+  const logout = () => {
+    setUser(initialValue);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  const authState = useContext(AuthContext);
+
+  return authState;
+};
