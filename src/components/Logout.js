53bd82932230754b785a2e4597af5062fa848668
vladislav99dev <vladislavcho99@abv.bg>
2022-08-19 17:50:31 +0300

    logout failing

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,27 +1,57 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext";
-import * as userRequester from "../services/userServices";
+import * as userServices from "../services/userServices";
 
 const Logout = () => {
   const { user, logout } = useAuthContext();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    userRequester
-      .logout(null, user.accessToken)
-      .then((res) => {
-        logout();
-        navigate("/");
-      })
-      .catch((err) => {
-        // should render components with error message
-        console.log(err);
-        alert(err.statusText);
-      });
-  }, []);
+  useEffect(()=> {
+    userServices.logout(null,user.accessToken)
+    .then((res)=> {
+      console.log(res);
+      // logout();
+      navigate("/");
+    })
+  },[])
 
   return null;
 };
 
 export default Logout;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // useEffect(() => {
+  //   userServices
+  //     .logout(null, user.accessToken)
+  //     .then((res) => {
+  //       logout();
+  //       navigate("/");
+  //     })
+  //     .catch((err) => {
+  //       // should render components with error message
+  //       console.log(err);
+  //       alert(err.statusText);
+  //     });
+  // }, []);
