5cfde320ef29db60aa1cfc57a8634f9621d286be
vladislav99dev <vladislavcho99@abv.bg>
2022-08-19 17:48:59 +0300

    register with validation context

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,53 +1,75 @@
-import { useNavigate,Link } from "react-router-dom";
-import * as userRequster from '../services/userServices'
+import { Link, useNavigate } from "react-router-dom";
+
+import * as userServices from "../services/userServices";
+import { validateRegister } from "../services/formValidationServices";
+import { useValidationsContext } from "../context/ValidationsContext";
+import ErrorMessage from "../partials/ErrorMessage";
+
 const Register = () => {
-    const navigate = useNavigate()
+  const { errors, clearErrors, setErrorsArr } = useValidationsContext();
+  const navigate = useNavigate();
 
+  const registerHandler = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
 
-    const registerHandler = (event) => {
-        event.preventDefault();
-        const formData  = new FormData(event.target);
-        const data = Object.fromEntries(formData);
+    const errorMessages = validateRegister(data);
 
-        if(data['password'] === data['confirm-password']){
-            userRequster.register(data)
-            .then((res) => {
-                navigate('/users/login')
-            })
-            .catch((err)=> {
-                console.log(err);
-            })
+    if (errorMessages.length > 0) {
+      setErrorsArr(errorMessages);
+    } else {
+      userServices.register(data).then((result) => {
+        if (result.message) {
+          setErrorsArr([result.message]);
+        } else {
+          clearErrors();
+          navigate("/users/login", { replace: true });
         }
-
+      });
+      clearErrors();
     }
+  };
 
+  return (
+    <section id="register-page" className="content auth">
+      <form id="register" onSubmit={registerHandler}>
+        <div className="container">
+          <div className="brand-logo"></div>
+          <h1>Register</h1>
+          {errors.length > 0
+            ? errors.map((err) => <ErrorMessage key={err} message={err} />)
+            : null}
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="maria@email.com"
+          />
 
-    return(
-        <section id="register-page" className="content auth">
-            <form id="register" onSubmit={registerHandler}>
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Register</h1>
+          <label htmlFor="pass">Password:</label>
+          <input type="password" name="password" id="register-password" />
 
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="maria@email.com"/>
+          <label htmlFor="con-pass">Confirm Password:</label>
+          <input
+            type="password"
+            name="confirm-password"
+            id="confirm-password"
+          />
 
-                    <label htmlFor="pass">Password:</label>
-                    <input type="password" name="password" id="register-password"/>
+          <input className="btn submit" type="submit" />
 
-                    <label htmlFor="con-pass">Confirm Password:</label>
-                    <input type="password" name="confirm-password" id="confirm-password"/>
+          <p className="field">
+            <span>
+              If you already have profile click{" "}
+              <Link to="/users/login">here</Link>
+            </span>
+          </p>
+        </div>
+      </form>
+    </section>
+  );
+};
 
-                    <input className="btn submit" type="submit" />
-
-                    <p className="field">
-                        <span>If you already have profile click <Link to="/users/login">here</Link></span>
-                    </p>
-                </div>
-            </form>
-        </section>
-    );
-}
-
-
-export default Register
+export default Register;
