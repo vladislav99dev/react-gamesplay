93012831f1c93f33221cfb95ce35769f3f10df51
vladislav99dev <vladislavcho99@abv.bg>
2022-08-19 17:48:45 +0300

    add validation context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import { AuthProvider } from "./context/AuthContext";
+import { ValidationProvider } from "./context/ValidationsContext";
 import Header from "./components/Header";
 import WelcomeWorld from "./components/WelcomeWorld/WelcomeWorld";
 import Login from "./components/Login";
@@ -9,32 +10,36 @@ import Catalog from "./components/GameCompontens/GameCatalog/Catalog";
 import Create from "./components/GameCompontens/Create";
 import Edit from "./components/GameCompontens/Edit";
 import Details from "./components/GameCompontens/Details";
-import Logout from "./components/Logout"
+import Logout from "./components/Logout";
+import React from "react";
 
 function App() {
   return (
     <div id="box">
-      <AuthProvider>
-        <Header />
-        <main id="main-content">
-          <Routes>
-            <Route path="/" element={<WelcomeWorld />}></Route>
+      <React.StrictMode>
+      <ValidationProvider>
+        <AuthProvider>
+          <Header />
+          <main id="main-content">
+            <Routes>
+              <Route path="/" element={<WelcomeWorld />}></Route>
 
-            <Route path="/users/">
-              <Route path="login" element={<Login />} />
-              <Route path="register" element={<Register />} />
-              <Route path="logout" element={<Logout />} />
-
-            </Route>
-            <Route path="/games/">
-              <Route path="catalog" element={<Catalog />} />
-              <Route path="create" element={<Create />} />
-              <Route path="edit" element={<Edit />} />
-              <Route path="details/:gameId" element={<Details />} />
-            </Route>
-          </Routes>
-        </main>
-      </AuthProvider>
+              <Route path="/users/">
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="logout" element={<Logout />} />
+              </Route>
+              <Route path="/games/">
+                <Route path="catalog" element={<Catalog />} />
+                <Route path="create" element={<Create />} />
+                <Route path="edit" element={<Edit />} />
+                <Route path="details/:gameId" element={<Details />} />
+              </Route>
+            </Routes>
+          </main>
+        </AuthProvider>
+      </ValidationProvider>
+      </React.StrictMode>
     </div>
   );
 }
