7d552f9c00ae1efa635d30af497546a23aa3a351
vladislav99dev <vladislavcho99@abv.bg>
2022-08-20 11:01:42 +0300

    details functionallity

--- src/components/GameCompontens/Details.js
+++ src/components/GameCompontens/Details.js
@@ -1,57 +1,63 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+
+import { useAuthContext } from "../../context/AuthContext";
 import * as gameService from "../../services/gameServices";
 
 const Details = () => {
   const [game, setGame] = useState({});
   const { gameId } = useParams();
 
+  const { user } = useAuthContext();
+
   useEffect(() => {
-      gameService.getOne(gameId)
-      .then((result) => setGame(result))
-  })
+    gameService.getOne(gameId).then((result) => setGame(result));
+  }, []);
+
+  const ownerButtons = () => (
+    <div className="buttons">
+      <Link to={`/games/edit/${gameId}`} className="button">
+        Edit
+      </Link>
+      <Link to={`/games/delete/${gameId}`} className="button">
+        Delete
+      </Link>
+    </div>
+  );
 
   return (
     <section id="game-details">
       <h1>Game Details</h1>
       <div className="info-section">
         <div className="game-header">
-          <img className="game-img" src={game.imageUrl} />
+          <img className="game-img" src={game.imageUrl} alt= "game img"/>
           <h1>{game.title}</h1>
           <span className="levels">MaxLevel: {game.maxLevel}</span>
           <p className="type">{game.category}</p>
         </div>
 
-        <p className="text">
-            {game.summary}
-        </p>
+        <p className="text">{game.summary}</p>
 
         <div className="details-comments">
           <h2>Comments:</h2>
-          {game.comments
-          ?
-          <ul>
-                <li className="comment">
+          {game.comments ? (
+            <ul>
+              <li className="comment">
                 <p>Content: I rate this one quite highly.</p>
-                </li>
-                <li className="comment">
+              </li>
+              <li className="comment">
                 <p>Content: The best game.</p>
-                </li>
-           </ul>
-          : <p className="no-comment">No comments.</p>
-          }
-
-
+              </li>
+            </ul>
+          ) : (
+            <p className="no-comment">No comments.</p>
+          )}
         </div>
-                {/*TODO buttons if owner*/} 
-        <div className="buttons">
-          <a href="#" className="button">
-            Edit
-          </a>
-          <a href="#" className="button">
-            Delete
-          </a>
-        </div>
+        {/*TODO buttons if owner*/}
+        {user._id === game._ownerId 
+        ? ownerButtons()
+        : null
+        }
       </div>
 
       <article className="create-comment">
