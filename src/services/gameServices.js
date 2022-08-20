be6f840789f03c746fede40b504b319de188f0f6
vladislav99dev <vladislavcho99@abv.bg>
2022-08-20 11:02:45 +0300

    abstraction function for edit and delete

--- src/services/gameServices.js
+++ src/services/gameServices.js
@@ -10,3 +10,36 @@ export const getOne = (gameId) => {
     response.json()
   );
 };
+
+export const gameRequester = (service, gameId, accessToken,data) => {
+  let options = {};
+  if (service === "DELETE") {
+    Object.assign(options, {
+      method: service,
+      headers: {
+        "content-type": "application/json",
+        "X-Authorization": accessToken,
+      },
+    });
+  } else if (service === "PUT") {
+    Object.assign(options, {
+      method: service,
+      headers: {
+        "content-type": "application/json",
+        "X-Authorization": accessToken,
+      },
+      body: JSON.stringify({
+        title: data.title,
+        category: data.category,
+        maxLevel: data.maxLevel,
+        imageUrl: data.imageUrl,
+        summary: data.summary,
+      }),
+    });
+  }
+
+  return fetch(`${url}/data/games/${gameId}`, options);
+};
+
+export const deleteOne = gameRequester.bind(null, "DELETE");
+export const editOne = gameRequester.bind(null, "PUT");
